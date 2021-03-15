ae6248800e8d470ca65c5d657c898ace1ca4147e
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2021-03-15 15:42:55 +0100

    initial commit

---
+++ src/main/java/com/example/merchteam/MerchTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.merchteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/merchteam/MerchTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.merchteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
