681e913a193d4a7aeccf6091fbf904ccefab829b
Mohamed Ghassen ELARBI <mohamedghassenelarbi@gmail.com>
2021-06-02 10:29:03 +0100

    updatePlanning

--- src/main/java/com/example/merchteam/planning/PlanningController.java
+++ src/main/java/com/example/merchteam/planning/PlanningController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.merchteam.appUser.AppUser;
 
 @RestController
 //@CrossOrigin(origins = "*" /*"http://localhost:3000"*/)
@@ -36,5 +39,9 @@ public void addPlanning(@RequestBody Planning planning) {
 @DeleteMapping(path = "{id}")
 public void deleteByMerchandiser(@PathVariable("id") Long id) {
 	planningService.deleteByMerchandiser(id);
+}
+@PutMapping(value = "/{id}")
+public void updatePlanning(@PathVariable Long id, @RequestBody Planning planning) {
+	planningService.updatePlanning(id, planning);
 }
 }
