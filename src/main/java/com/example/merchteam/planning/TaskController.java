5102afce3b7336b1afac565ad6dad26a3eca5ba0
Mohamed Ghassen ELARBI <mohamedghassenelarbi@gmail.com>
2021-06-03 12:04:21 +0100

    implementing update for claimType

--- src/main/java/com/example/merchteam/planning/TaskController.java
+++ src/main/java/com/example/merchteam/planning/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.merchteam.planning;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,8 +44,8 @@ public class TaskController {
 	@PutMapping("{id}")
 	public void updateTaskById(
 		@PathVariable("id") Long id,
-		@RequestBody String state
+		@RequestBody Map<String,String> body
 	) {
-		taskService.updateTaskById(id, state);
+		taskService.updateTaskById(id, body.get("state"));
 	}
 }
